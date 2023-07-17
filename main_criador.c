6fe29ea04e14c5ef2a2f07a641ad6a6273f4b6c6
Fabrício Liedson <111002008+Liedsonfsa@users.noreply.github.com>
2023-07-17 12:09:21 -0300

    Create main_criador.c

---
+++ main_criador.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "criador.h"
+#include "criador.c"
+
+int main() {
+
+    Criador *c;
+
+    c = criarListaDuplaCriadores();
+
+    c = cadastrarCriador(c);
+    c = cadastrarCriador(c);
+    c = cadastrarCriador(c);
+    c = cadastrarCriador(c);
+    
+    mostrarCriadores(c);
+
+    ///printf("%d\n",temFazenda(c->fazendas));
+    
+
+    return 0;
+}
