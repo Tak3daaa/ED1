c2a7aad6b58d3361107b7da66dcbd1053126daa9
Tak3daaa <119462619+Tak3daaa@users.noreply.github.com>
2023-07-22 11:54:10 -0300

    Update animal.h

--- animal.h
+++ animal.h
@@ -4,9 +4,8 @@ Animal *criaListaEncadeadaSimplesAnimais();
 Animal *cadastrarAnimal(Animal *rebanho);
 Animal *buscarAnimal(Animal *rebanho);
 Animal *removerAnimal(Fazenda *fazenda, int id_animal);
-Animal *permutasAnimais(Fazenda *origem, Fazenda *destino, int id_animal);//obervar para atualizar o status do animal na fazenda de destino
-Animal *removerAnimal(Fazenda *fazenda, int id_animal); 
-
+void permutasAnimais(Fazenda *origem, Fazenda *destino);//obervar para atualizar o status do animal na fazenda de destino
+void trocarAnimal(Animal* animal1, Animal* animal2);
 int animalExiste(Animal* rebanho, int id_animal);
 void liberarmemoriaAnimal(Animal *rebanho);
 void contAnimaisSexo(Fazenda *fazenda);
