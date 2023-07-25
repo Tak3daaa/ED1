f49569193adfb3c504e4102bcfd0248b2f48ea4d
Tak3daaa <119462619+Tak3daaa@users.noreply.github.com>
2023-07-25 08:43:18 -0300

    Update animal.h

--- animal.h
+++ animal.h
@@ -1,15 +1,15 @@
 typedef struct animal Animal; 
 
-Animal *criaListaEncadeadaSimplesAnimais();
-Animal *cadastrarAnimal(Animal *rebanho);
-Animal *buscarAnimal(Animal *rebanho);
-Animal *removerAnimal(Fazenda *fazenda, int id_animal);
-void permutasAnimais(Fazenda *origem, Fazenda *destino);//obervar para atualizar o status do animal na fazenda de destino
-void trocarAnimal(Animal* animal1, Animal* animal2);
-int animalExiste(Animal* rebanho, int id_animal);
-void liberarmemoriaAnimal(Animal *rebanho);
-void contAnimaisSexo(Fazenda *fazenda);
+Animal *criaListaEncadeadaSimplesAnimais(); //
+Animal *cadastrarAnimal(Animal *rebanho); // 
+Animal *buscarAnimal(Animal *rebanho); //
+void permutasAnimais(Fazenda *origem, Fazenda *destino);// 
+Animal *removerAnimal(Fazenda *fazenda, int id_animal); // testar
+
+int animalExiste(Animal* rebanho, int id_animal); //
+void liberarmemoriaAnimal(Animal *rebanho); //
+void contAnimaisSexo(Fazenda *fazenda); //
 void MostrarStatus(Fazenda *fazenda, int sts);
-void mostrarAnimal(Animal *rebanho);
-void mostrarTodosAnimais(Fazenda *fazenda);
-float contArroba(Fazenda *fazenda);
+void mostrarAnimal(Animal *rebanho); //
+void mostrarTodosAnimais(Fazenda *fazenda); //
+float contArroba(Fazenda *fazenda); //
